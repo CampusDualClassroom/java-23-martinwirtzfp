1114ad64791b8eb75cfafa99923a2b57ea8c949f
Martin Sanchez Novo <martinwirtzfp@gmail.com>
2025-05-26 10:26:00 +0200

    Ejercicio completado y testeado

--- src/main/java/com/campusdual/classroom/Exercise23.java
+++ src/main/java/com/campusdual/classroom/Exercise23.java
@@ -6,15 +6,28 @@ import java.util.Map;
 public class Exercise23 {
 
     public static Map<String, Person> createHashMap() {
-
+        Map<String, Person> peopleMap = new HashMap<>();
+        peopleMap.put("person", new Person("John", "Smith"));
+        peopleMap.put("teacher", new Teacher("María","Montessori", "Educación"));
+        peopleMap.put("police", new PoliceOfficer("Jake", "Peralta", "B-99"));
+        peopleMap.put("doctor", new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return peopleMap;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
-
+        map.put(key,value);
+        return value;
     }
 
     public static void printMapValues(Map<String, Person> map) {
-
+        for(Map.Entry<String, Person> entry: map.entrySet()){
+            entry.getValue().getDetails();
+        }
     }
-    
+
+    public static void main(String[] args) {
+        Map<String, Person> map = createHashMap();
+        addMapValue(map, "police", new PoliceOfficer("Charles", "Boyle", "B-99"));
+    }
+
 }
